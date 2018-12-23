649bcb874fb5b158f725febcc007a5dcff688ff9
Stephan Griesel <sgriesel@gmail.com>
2018-12-23 07:26:41 +0100

    set up routing

--- members-only/src/App.js
+++ members-only/src/App.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/layout/NavBar";
+import Home from "./components/pages/Home";
+import Members from "./components/pages/Members";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <div className="container">
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/members" exact={true} component={Members} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
