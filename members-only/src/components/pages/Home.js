3ac1074f36db6fd6583ea04827019c04b037fd76
Stephan Griesel <sgriesel@gmail.com>
2018-12-23 07:59:24 +0100

    add login & signin widget

--- members-only/src/components/pages/Home.js
+++ members-only/src/components/pages/Home.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withAuth } from '@okta/okta-react';
 
-export default () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
-};
+export default withAuth(class Home extends Component {
+    state = { authenticated: null };
+
+  checkAuthentication = async() => {
+    const authenticated = await this.props.auth.isAuthenticated();
+    if (authenticated !== this.state.authenticated) {
+      this.setState({ authenticated });
+    }
+  }
+
+  async componentDidMount() {
+    this.checkAuthentication();
+  }
+
+  async componentDidUpdate() {
+    this.checkAuthentication();
+  }
+
+  login = async() => {
+    this.props.auth.login('/');
+  }
+
+ logout =  async() => {
+    this.props.auth.logout('/');
+  }
+
+  render() {
+    if (this.state.authenticated === null) return null;
+
+    const button = this.state.authenticated ?
+      <button onClick={this.logout}>Logout</button> :
+      <button onClick={this.login}>Login</button>;
+
+    return (
+      <div>
+       
+      </div>
+    );
+  }
+});
