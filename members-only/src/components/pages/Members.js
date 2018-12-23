bf51282a512c2464049a8a21503d21ab7fa9ea8a
Stephan Griesel <sgriesel@gmail.com>
2018-12-23 08:31:34 +0100

    add members page & details

--- members-only/src/components/pages/Members.js
+++ members-only/src/components/pages/Members.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default () => {
-  return (
-    <div>
-      <h1>Members</h1>
-    </div>
-  );
-};
+class Members extends Component {
+    state = {
+        currentUserName:'',
+        currentUserEmail:''
+    }
+
+    componentDidMount(){
+        const idToken = JSON.parse(localStorage.getItem('okta-token-storage'));
+        this.setState({
+            currentUserEmail: idToken.idToken.claims.email,
+            currentUserName: idToken.idToken.claims.name,
+            authTime: idToken.idToken.claims.auth_time
+        });
+    }
+
+  render() {
+      const { currentUserEmail, currentUserName, authTime } = this.state;
+    return (
+        <div>
+            <h1>Welcome {currentUserName}</h1>
+            <p>Email: {currentUserEmail}</p>
+            <p>You are in the members only area</p>
+            <h3>You're authorized time: {authTime}</h3>
+        </div>
+    )
+  }
+}
+
+export default Members;
